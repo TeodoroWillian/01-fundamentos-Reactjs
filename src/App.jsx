dc62b930542f344ad65aff0b6843bb813ab07010
TeodoroWillian <totawillian@gmail.com>
2022-08-20 14:39:45 -0400

    estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import {Post} from './Post';
+import {Header} from './components/Header'
+import './global.css';
 export function App() {
 
   return (
-    <Post />
+    <div>
+
+    <Header />
+
+    <Post 
+    author="Willian Teodoro" 
+    content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati necessitatibus amet voluptas sequi porro?" 
+    />
+    <Post 
+    author="Cabrito Tevez"
+    content="Um novo content"
+    />
+    </div>
+
   )
 }
